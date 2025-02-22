2867a45537e9200adfd0e4739d8ddb8f4cf7c1ca
sachin kumar <sachinmehtab@gmail.com>
2025-02-22 23:32:15 +0530

    Initial Commit

---
+++ NotingDownDesktop/NotingDownDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotingDownDesktopApp.swift
+//  NotingDownDesktop
+//
+//  Created by sachin kumar on 22/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotingDownDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
